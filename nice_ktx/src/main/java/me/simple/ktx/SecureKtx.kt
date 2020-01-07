ea1d9d0d884077a17ec013f37e891c040d20c55f
陈鹏 <simple19930611@gmail.com>
2020-01-07 19:00:44 +0800

    add:SecureKtx

---
+++ nice_ktx/src/main/java/me/simple/ktx/SecureKtx.kt
@@ -0,0 +1,26 @@
+package me.simple.ktx
+
+val String.md5: String
+    get() {
+        return this + ""
+    }
+
+val String.hash: String
+    get() {
+        return ""
+    }
+
+val String.sha1: String
+    get() {
+        return ""
+    }
+
+val String.sha256: String
+    get() {
+        return ""
+    }
+
+val String.sha512: String
+    get() {
+        return ""
+    }
