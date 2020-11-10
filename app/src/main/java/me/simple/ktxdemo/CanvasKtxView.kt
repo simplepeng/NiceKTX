0ce5c61d25f8f9bb56b821fb6b15c5a4e8820286
陈鹏 <simple19930611@gmail.com>
2020-11-10 10:56:12 +0800

    更新：CanvasKtx->drawTextOnTop,drawTextOnBottom

---
+++ app/src/main/java/me/simple/ktxdemo/CanvasKtxView.kt
@@ -0,0 +1,31 @@
+package me.simple.ktxdemo
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import me.simple.ktx.drawTextOnBottom
+import me.simple.ktx.drawTextOnTop
+
+class CanvasKtxView
+@JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private val mPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textSize = 50f
+        color = Color.BLACK
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        canvas.drawTextOnTop("HaHa", 0f, 0f, mPaint)
+
+        canvas.drawTextOnBottom("My", 0f, height.toFloat(), mPaint)
+    }
+}
--- nice_ktx/src/main/java/me/simple/ktx/CanvasKTX.kt
+++ nice_ktx/src/main/java/me/simple/ktx/CanvasKTX.kt
@@ -1,7 +1,32 @@
 package me.simple.ktx
 
 import android.graphics.Canvas
+import android.graphics.Paint
 
-fun Canvas.drawTextOnLeftTop(){
-//    this.drawText()
+/**
+ * 画文字-以顶部为基准线
+ */
+fun Canvas.drawTextOnTop(
+    text: String,
+    x: Float,
+    y: Float,
+    paint: Paint
+) {
+    val fontMetrics = paint.fontMetrics
+    val newY = y - fontMetrics.top
+    this.drawText(text, x, newY, paint)
+}
+
+/**
+ * 画文字-以底部为基准线
+ */
+fun Canvas.drawTextOnBottom(
+    text: String,
+    x: Float,
+    y: Float,
+    paint: Paint
+) {
+    val fontMetrics = paint.fontMetrics
+    val newY = y - fontMetrics.bottom
+    this.drawText(text, x, newY, paint)
 }
