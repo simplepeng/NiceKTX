ed17d6d631ef6606c1cd9cbe4328d2893653c650
chenpeng <383559698@qq.com>
2021-02-07 20:01:20 +0800

    TextViewKTX.kt setTextColor

--- nice_ktx/src/main/java/me/simple/ktx/TextViewKTX.kt
+++ nice_ktx/src/main/java/me/simple/ktx/TextViewKTX.kt
@@ -1,10 +1,17 @@
 package me.simple.ktx
 
+import android.graphics.Color
 import android.widget.TextView
+import ktx.common.Desc
 
 /**
  * 清空文本
  */
 fun TextView.clearText() {
     this.text = ""
+}
+
+@Desc("设置文本颜色","1.0.1")
+fun TextView.setTextColor(colorStr: String) {
+    this.setTextColor(Color.parseColor(colorStr))
 }
