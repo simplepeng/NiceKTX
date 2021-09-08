cbf25ab3c68699c8fc8f4b78fc32036417398db0
chenpeng <383559698@qq.com>
2021-09-08 11:25:21 +0800

    drawTextOnCenter

--- nice_ktx/src/main/java/me/simple/ktx/CanvasKT.kt
+++ nice_ktx/src/main/java/me/simple/ktx/CanvasKT.kt
@@ -4,7 +4,7 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import ktx.common.Desc
 
-@Desc("画文字-以顶部为基准线","v1.0.1")
+@Desc("画文字-以顶部为基准线", "v1.0.1")
 fun Canvas.drawTextOnTop(
     text: String,
     x: Float,
@@ -16,7 +16,7 @@ fun Canvas.drawTextOnTop(
     this.drawText(text, x, newY, paint)
 }
 
-@Desc("画文字-以底部为基准线","v1.0.1")
+@Desc("画文字-以底部为基准线", "v1.0.1")
 fun Canvas.drawTextOnBottom(
     text: String,
     x: Float,
@@ -26,4 +26,19 @@ fun Canvas.drawTextOnBottom(
     val fontMetrics = paint.fontMetrics
     val newY = y - fontMetrics.bottom
     this.drawText(text, x, newY, paint)
+}
+
+@Desc("画文字-以中心点为基准", "v1.0.4")
+fun Canvas.drawTextOnCenter(
+    text: String,
+    x: Float,
+    y: Float,
+    paint: Paint
+) {
+    val tmpAlign = paint.textAlign
+    val fm = paint.fontMetrics
+    val newY = y + ((fm.descent - fm.ascent) / 2 - fm.descent)
+    paint.textAlign = Paint.Align.CENTER
+    this.drawText(text, x, newY, paint)
+    paint.textAlign = tmpAlign
 }
