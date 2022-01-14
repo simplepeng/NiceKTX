5587e881ebab6d2ff0aca2608fcffa66ca85b23c
simple <383559698@qq.com>
2022-01-14 14:23:52 +0800

    DefaultKT.kt

--- app/src/main/java/me/simple/ktxdemo/BaseActivity.kt
+++ app/src/main/java/me/simple/ktxdemo/BaseActivity.kt
@@ -2,6 +2,7 @@ package me.simple.ktxdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import me.simple.ktx.orDefault
 
 open class BaseActivity : AppCompatActivity() {
 
---
+++ app/src/test/java/me/simple/ktxdemo/DefaultKTUnitTest.kt
@@ -0,0 +1,23 @@
+package me.simple.ktxdemo
+
+import me.simple.ktx.orDefault
+import org.junit.Test
+
+class DefaultKTUnitTest {
+
+    @Test
+    fun allTest() {
+
+        val bool: Boolean? = null
+        println("bool = ${bool.orDefault(false)}")
+
+        val str: String? = null
+        println("str = ${str.orDefault("haha")}")
+
+        val intVale: Int? = null
+        println("intVale = ${intVale.orDefault(1)}")
+
+        val floatVale: Float? = null
+        println("floatVale = ${floatVale.orDefault(2)}")
+    }
+}
