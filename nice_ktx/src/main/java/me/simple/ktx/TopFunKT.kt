64df658ca938f78381a64a5249e60e0d7f98f862
simple <383559698@qq.com>
2023-04-04 23:51:26 +0800

    update TopFunKT.kt

--- nice_ktx/src/main/java/me/simple/ktx/TopFunKT.kt
+++ nice_ktx/src/main/java/me/simple/ktx/TopFunKT.kt
@@ -1,22 +1,2 @@
 package me.simple.ktx
 
-fun <A> ifNotNull(
-    a: A?,
-    block: (A) -> Unit
-) {
-    if (a != null) block(a)
-}
-
-fun <A, B> ifNotNull(
-    a: A, b: B,
-    block: (arg1: A, arg2: B) -> Unit
-) {
-    if (a != null && b != null) block(a, b)
-}
-
-fun <A, B, C> ifNotNull(
-    a: A, b: B, c: C,
-    block: (arg1: A, arg2: B, arg3: C) -> Unit
-) {
-    if (a != null && b != null && c != null) block(a, b, c)
-}
