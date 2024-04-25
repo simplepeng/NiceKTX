9da128bfe1ec59ba552873c2524ae3214a9b1443
simple <383559698@qq.com>
2024-04-25 16:33:43 +0800

    update IntentKTX.kt

--- nice_ktx/src/main/java/me/simple/ktx/IntentKTX.kt
+++ nice_ktx/src/main/java/me/simple/ktx/IntentKTX.kt
@@ -6,23 +6,19 @@ import android.os.Parcelable
 import java.io.Serializable
 
 @Suppress("DEPRECATION")
-inline fun <reified T : Serializable> Intent.getSerializableExtraCompat(
-    name: String
-): T? {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        this.getSerializableExtra(name, T::class.java)
-    } else {
-        this.getSerializableExtra(name) as? T
-    }
+inline fun <reified T : Serializable> Intent.getSerializableExtraCompat(name: String): T? {
+  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    this.getSerializableExtra(name, T::class.java)
+  } else {
+    this.getSerializableExtra(name) as? T
+  }
 }
 
 @Suppress("DEPRECATION")
-inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(
-    name: String
-): T? {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        this.getParcelableExtra(name, T::class.java)
-    } else {
-        this.getParcelableExtra(name) as? T
-    }
+inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(name: String): T? {
+  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    this.getParcelableExtra(name, T::class.java)
+  } else {
+    this.getParcelableExtra(name) as? T
+  }
 }
