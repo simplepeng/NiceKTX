323ecf8eb898c9dc629bd9851d2e39e973df8f8a
simple <383559698@qq.com>
2024-04-26 18:47:35 +0800

    update ViewPager2KT.kt

--- viewpager_ktx/src/main/java/me/simple/ktx/viewpager/ViewPager2KT.kt
+++ viewpager_ktx/src/main/java/me/simple/ktx/viewpager/ViewPager2KT.kt
@@ -3,18 +3,26 @@ package me.simple.ktx.viewpager
 import androidx.viewpager2.widget.ViewPager2
 import ktx.common.Desc
 
-@Desc(desc = "下一页", createVersion = "v1.0.4")
+/**
+ * 下一页
+ * @param smoothScroll 是否平滑滚动
+ * @since 1.0.4
+ */
 fun ViewPager2.nextPage(smoothScroll: Boolean = true) {
-    if (this.adapter == null) return
-    val nextItem = this.currentItem + 1
-    if (nextItem >= this.adapter!!.itemCount) return
-    this.setCurrentItem(nextItem, smoothScroll)
+  if (this.adapter == null) return
+  val nextItem = this.currentItem + 1
+  if (nextItem >= this.adapter!!.itemCount) return
+  this.setCurrentItem(nextItem, smoothScroll)
 }
 
-@Desc(desc = "上一页", createVersion = "v1.0.4")
+/**
+ * 上一页
+ * @param smoothScroll 是否平滑滚动
+ * @since 1.0.4
+ */
 fun ViewPager2.prePage(smoothScroll: Boolean = true) {
-    if (this.adapter == null) return
-    val preItem = this.currentItem - 1
-    if (preItem < 0) return
-    this.setCurrentItem(preItem, smoothScroll)
+  if (this.adapter == null) return
+  val preItem = this.currentItem - 1
+  if (preItem < 0) return
+  this.setCurrentItem(preItem, smoothScroll)
 }
