9d3ea6db4e6bdc5b29a27f9ae725bde7083ec30f
simple <383559698@qq.com>
2024-04-26 15:51:21 +0800

    update ActivityKT.kt

--- nice_ktx/src/main/java/me/simple/ktx/ActivityKT.kt
+++ nice_ktx/src/main/java/me/simple/ktx/ActivityKT.kt
@@ -5,12 +5,17 @@ import android.content.Intent
 import ktx.common.Desc
 import kotlin.reflect.KClass
 
+
+/**
+ * ActivityKT Activity的扩展
+ */
+
 /**
  * 启动一个Activity
  * @param clazz 要启动的Activity类
  * @since 1.0.2
  */
-@Desc("启动一个Activity","1.0.2")
+@Desc("启动一个Activity", "1.0.2")
 inline fun <reified T : Activity> Activity.startActivity(clazz: KClass<T>) {
     this.startActivity(Intent(this, clazz.java))
 }
--- nice_ktx/src/main/java/me/simple/ktx/helpers/BitmapHelper.kt
+++ nice_ktx/src/main/java/me/simple/ktx/helpers/BitmapHelper.kt
@@ -7,6 +7,7 @@ import android.graphics.drawable.BitmapDrawable
 
 /**
  * @hide
+ * @suppress
  */
 object BitmapHelper {
 
