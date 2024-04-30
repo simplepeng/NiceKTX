b16438cd5a5bcc00d5d52775fa76fae4f59ed055
simple <383559698@qq.com>
2024-04-30 10:59:24 +0800

    update EventBusKT.kt

--- eventbus_ktx/src/main/java/me/simple/ktx/eventbus/EventBusKT.kt
+++ eventbus_ktx/src/main/java/me/simple/ktx/eventbus/EventBusKT.kt
@@ -1,15 +1,22 @@
 package me.simple.ktx.eventbus
 
-import androidx.lifecycle.*
-import ktx.common.Desc
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import org.greenrobot.eventbus.EventBus
 
-@Desc("在onCreate中注册事件，自动在onDestroy中注销事件")
+/**
+ * 在onCreate中注册事件，自动在onDestroy中注销事件
+ *
+ * @param owner LifecycleOwner，可以是Activity或Fragment
+ */
 fun <T : LifecycleOwner> EventBus.registerOnCreate(owner: T) {
     if (this.isRegistered(owner)) return
     owner.lifecycle.addObserver(OnCreateLifecycleObserver(this, owner))
 }
 
+/**
+ * @suppress
+ */
 class OnCreateLifecycleObserver<T>(
     private val eventBus: EventBus,
     private val owner: T
@@ -28,12 +35,19 @@ class OnCreateLifecycleObserver<T>(
     }
 }
 
-@Desc("在onStart中注册事件，自动在onStop中注销事件")
+/**
+ * 在onStart中注册事件，自动在onStop中注销事件
+ *
+ * @param owner LifecycleOwner，可以是Activity或Fragment
+ */
 fun <T : LifecycleOwner> EventBus.registerOnStart(owner: T) {
     if (this.isRegistered(owner)) return
     owner.lifecycle.addObserver(OnStartLifecycleObserver(this, owner))
 }
 
+/**
+ * @suppress
+ */
 class OnStartLifecycleObserver<T>(
     private val eventBus: EventBus,
     private val owner: T
@@ -52,12 +66,19 @@ class OnStartLifecycleObserver<T>(
     }
 }
 
-@Desc("在onResume中注册事件，自动在onPause中注销事件")
+/**
+ * 在onResume中注册事件，自动在onPause中注销事件
+ *
+ * @param owner LifecycleOwner，可以是Activity或Fragment
+ */
 fun <T : LifecycleOwner> EventBus.registerOnResume(owner: T) {
     if (this.isRegistered(owner)) return
     owner.lifecycle.addObserver(OnResumeLifecycleObserver(this, owner))
 }
 
+/**
+ * @suppress
+ */
 class OnResumeLifecycleObserver<T>(
     private val eventBus: EventBus,
     private val owner: T
@@ -76,20 +97,28 @@ class OnResumeLifecycleObserver<T>(
     }
 }
 
-@Desc("安全调用注册方法")
+/**
+ * 安全的调用注册方法
+ */
 fun EventBus.safeRegister(obj: Any) {
     if (this.isRegistered(obj)) return
     this.register(obj)
 }
 
-@Desc("安全调用注销方法")
+/**
+ * 安全的调用注销方法
+ */
 fun EventBus.safeUnregister(obj: Any) {
     if (!this.isRegistered(obj)) return
     this.unregister(obj)
 }
 
-@Desc("发布事件")
+/**
+ * 发布事件
+ */
 fun postEvent(event: Any) = EventBus.getDefault().post(event)
 
-@Desc("发布粘性事件")
+/**
+ * 发布粘性事件
+ */
 fun postStickyEvent(event: Any) = EventBus.getDefault().postSticky(event)
