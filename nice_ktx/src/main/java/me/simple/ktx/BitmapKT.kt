66d6c5e99aec974ca9226fd0a3785bdd8e05ec0a
simple <383559698@qq.com>
2024-05-09 17:15:46 +0800

    update BitmapKT.kt

--- nice_ktx/src/main/java/me/simple/ktx/BitmapKT.kt
+++ nice_ktx/src/main/java/me/simple/ktx/BitmapKT.kt
@@ -1,8 +1,12 @@
 package me.simple.ktx
 
-import android.graphics.*
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Matrix
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.Base64
-import ktx.common.Desc
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
@@ -16,65 +20,96 @@ import java.io.FileOutputStream
 
 /**
  * 保存到文件
+ *
  * @param path 保存路径
  * @param format 保存格式
  * @param quality 保存质量
  *
  * @since 1.0.1
  */
-@Desc("Bitmap转File", "v1.0.1")
 fun Bitmap.saveFile(
-  path: String,
-  format: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG,
-  quality: Int = 100
+    path: String,
+    format: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG,
+    quality: Int = 100
 ) {
-  val file = File(path)
-  val out = FileOutputStream(file)
-  this.compress(format, quality, out)
-  out.flush()
-  out.close()
+    val file = File(path)
+    val out = FileOutputStream(file)
+    this.compress(format, quality, out)
+    out.flush()
+    out.close()
 }
 
-@Desc("Bitmap转base64", "v1.0.1", "v1.0.4", "支持flags")
+/**
+ * Bitmap转base64
+ *
+ * @param quality 保存质量
+ * @param format 保存格式
+ * @param flags 标志位
+ *
+ * @since 1.0.1
+ */
 fun Bitmap.base64(
-  quality: Int = 100,
-  format: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG,
-  flags: Int = Base64.DEFAULT
+    quality: Int = 100,
+    format: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG,
+    flags: Int = Base64.DEFAULT
 ): String {
-  val bos = ByteArrayOutputStream()
-  this.compress(format, quality, bos)
-  val bytes = bos.toByteArray()
+    val bos = ByteArrayOutputStream()
+    this.compress(format, quality, bos)
+    val bytes = bos.toByteArray()
 
-  val encode = Base64.encode(bytes, flags)
-  return String(encode)
+    val encode = Base64.encode(bytes, flags)
+    return String(encode)
 }
 
-@Desc("base64转Bitmap", "v1.0.4")
-fun String.toBitmap(flags: Int = Base64.DEFAULT): Bitmap {
-  val bytes = Base64.decode(this, flags)
-  return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-}
-
-@Desc("按比例缩放Bitmap", "v1.0.4")
-fun Bitmap.scale(
-  sx: Float,
-  sy: Float,
-  filter: Boolean = false
+/**
+ * base64转Bitmap
+ *
+ * @param flags 标志位
+ *
+ * @since 1.0.4
+ */
+fun String.toBitmap(
+    flags: Int = Base64.DEFAULT
 ): Bitmap {
-  val matrix = Matrix()
-  matrix.postScale(sx, sy)
-  return Bitmap.createBitmap(this, 0, 0, this.width, this.height, matrix, filter)
+    val bytes = Base64.decode(this, flags)
+    return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
 }
 
-@Desc("按大小缩放Bitmap", "v1.0.4")
+/**
+ * 按比例缩放Bitmap
+ *
+ * @param sx 缩放比例
+ * @param sy 缩放比例
+ * @param filter
+ *
+ * @since 1.0.4
+ */
 fun Bitmap.scale(
-  newWidth: Int,
-  newHeight: Int,
-  filter: Boolean = false
+    sx: Float,
+    sy: Float,
+    filter: Boolean = false
 ): Bitmap {
-  val sx = newWidth.toFloat() / this.width
-  val sy = newHeight.toFloat() / this.height
-  return scale(sx, sy, filter)
+    val matrix = Matrix()
+    matrix.postScale(sx, sy)
+    return Bitmap.createBitmap(this, 0, 0, this.width, this.height, matrix, filter)
+}
+
+/**
+ * 按大小缩放Bitmap
+ *
+ * @param newWidth 缩放后的宽
+ * @param newHeight 缩放后的高
+ *
+ * @since 1.0.4
+ */
+fun Bitmap.scale(
+    newWidth: Int,
+    newHeight: Int,
+    filter: Boolean = false
+): Bitmap {
+    val sx = newWidth.toFloat() / this.width
+    val sy = newHeight.toFloat() / this.height
+    return scale(sx, sy, filter)
 }
 
 /**
@@ -90,17 +125,17 @@ fun Bitmap.scale(
  * @since 1.0.4
  */
 fun Bitmap.addWatermark(
-  watermark: Bitmap,
-  x: Float,
-  y: Float,
-  markW: Int = watermark.width,
-  markH: Int = watermark.height,
-  alpha: Int = 255
+    watermark: Bitmap,
+    x: Float,
+    y: Float,
+    markW: Int = watermark.width,
+    markH: Int = watermark.height,
+    alpha: Int = 255
 ) {
-  val canvas = Canvas(this)
-  val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-  paint.alpha = alpha
+    val canvas = Canvas(this)
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    paint.alpha = alpha
 //    canvas.drawBitmap(watermark, x, y, paint)
-  val dstRect = RectF(x, y, x + markW, y + markH)
-  canvas.drawBitmap(watermark, null, dstRect, paint)
+    val dstRect = RectF(x, y, x + markW, y + markH)
+    canvas.drawBitmap(watermark, null, dstRect, paint)
 }
