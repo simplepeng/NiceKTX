ee667c267324d704965b3fb4af6d88be43e2e369
simple <383559698@qq.com>
2024-05-10 15:45:35 +0800

    update DefaultKT.kt

--- nice_ktx/src/main/java/me/simple/ktx/DefaultKT.kt
+++ nice_ktx/src/main/java/me/simple/ktx/DefaultKT.kt
@@ -1,23 +1,38 @@
 package me.simple.ktx
 
-import ktx.common.Desc
 
-@Desc("布尔值的默认值填充", "v1.0.4")
+/**
+ * 布尔值的默认值填充
+ *
+ * @since 1.0.4
+ */
 fun Boolean?.orDefault(defValue: Boolean = true): Boolean {
     return this ?: defValue
 }
 
-@Desc("字符串的默认值填充", "v1.0.4")
+/**
+ * 字符串的默认值填充
+ *
+ * @since 1.0.4
+ */
 fun String?.orDefault(defValue: String = ""): String {
     return this ?: defValue
 }
 
-@Desc("数值类型的默认值填充", "v1.0.4")
+/**
+ * 数值类型的默认值填充
+ *
+ * @since 1.0.4
+ */
 fun <T : Number> T?.orDefault(defValue: T): T {
     return this ?: defValue
 }
 
-@Desc("默认值填充", "v1.0.4")
+/**
+ * 默认值填充
+ *
+ * @since 1.0.4
+ */
 fun <T> T?.orDefault(defValue: T): T {
     return this ?: defValue
 }
