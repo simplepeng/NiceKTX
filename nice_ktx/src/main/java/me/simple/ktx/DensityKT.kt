967dd90ac4311670a6e87b2358dc9c7546b09118
simple <383559698@qq.com>
2024-05-10 16:48:24 +0800

    update DensityKT.kt

--- nice_ktx/src/main/java/me/simple/ktx/DensityKT.kt
+++ nice_ktx/src/main/java/me/simple/ktx/DensityKT.kt
@@ -6,106 +6,221 @@ import android.content.res.Resources
 import android.util.TypedValue
 import android.view.View
 import androidx.fragment.app.Fragment
-import ktx.common.Desc
 
 //---------------
 
-@Desc("Resources dp转px", "1.0.2")
-fun Resources.dp2px(value: Float) =
-    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, this.displayMetrics)
+/**
+ * Resources -- dp转px
+ *
+ * @since 1.0.2
+ */
+fun Resources.dp2px(value: Float) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, this.displayMetrics)
 
-@Desc("Resources sp转px", "1.0.2")
-fun Resources.sp2px(value: Float) =
-    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, value, this.displayMetrics)
+/**
+ * Resources -- sp转px
+ *
+ * @since 1.0.2
+ */
+fun Resources.sp2px(value: Float) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, value, this.displayMetrics)
 
-@Desc("Resources px转dp", "1.0.4")
+/**
+ * Resources -- px转dp
+ *
+ * @since 1.0.4
+ */
 fun Resources.px2dp(pxValue: Float) = pxValue / this.displayMetrics.density
 
-@Desc("Resources px转sp", "1.0.4")
+/**
+ * Resources -- px转sp
+ *
+ * @since 1.0.4
+ */
 fun Resources.px2sp(pxValue: Float) = pxValue / this.displayMetrics.scaledDensity
 
 //---------------
 
-@Desc("View dp转px", "1.0.2")
+/**
+ * View -- dp转px
+ *
+ * @since 1.0.2
+ */
 fun View.dp2px(value: Float) = this.resources.dp2px(value)
 
-@Desc("View sp转px", "1.0.2")
+/**
+ * View -- sp2px
+ *
+ * @since 1.0.2
+ */
 fun View.sp2px(value: Float) = this.resources.sp2px(value)
 
-@Desc("View px转dp", "1.0.4")
+/**
+ * View -- px转dp
+ *
+ * @since 1.0.4
+ */
 fun View.px2dp(pxValue: Float) = this.resources.px2dp(pxValue)
 
-@Desc("View px转sp", "1.0.4")
+/**
+ * View -- px转sp
+ *
+ * @since 1.0.4
+ */
 fun View.px2sp(pxValue: Float) = this.resources.px2sp(pxValue)
 
 //---------------
 
-@Desc("Context dp转px", "1.0.2")
+/**
+ * Context -- dp转px
+ *
+ * @since 1.0.2
+ */
 fun Context.dp2px(value: Float) = this.resources.dp2px(value)
 
-@Desc("Context sp转px", "1.0.2")
+/**
+ * Context -- sp转px
+ *
+ * @since 1.0.2
+ */
 fun Context.sp2px(value: Float) = this.resources.sp2px(value)
 
-@Desc("Context px转dp", "1.0.4")
+/**
+ * Context -- px2dp
+ *
+ * @since 1.0.4
+ */
 fun Context.px2dp(pxValue: Float) = this.resources.px2dp(pxValue)
 
-@Desc("Context px转sp", "1.0.4")
+/**
+ * Context -- px转sp
+ *
+ * @since 1.0.4
+ */
 fun Context.px2sp(pxValue: Float) = this.resources.px2sp(pxValue)
 
 //---------------
 
-@Desc("Context dp转px", "1.0.2")
+/**
+ * Fragment -- dp转px
+ *
+ * @since 1.0.2
+ */
 fun Fragment.dp2px(value: Float) = this.resources.dp2px(value)
 
-@Desc("Context sp转px", "1.0.2")
+/**
+ * Fragment -- sp转px
+ *
+ * @since 1.0.2
+ */
 fun Fragment.sp2px(value: Float) = this.resources.sp2px(value)
 
-@Desc("Context px转dp", "1.0.4")
+/**
+ * Fragment -- px转dp
+ *
+ * @since 1.0.4
+ */
 fun Fragment.px2dp(pxValue: Float) = this.resources.px2dp(pxValue)
 
-@Desc("Context px转sp", "1.0.4")
+/**
+ * Fragment -- px转sp
+ *
+ * @since 1.0.4
+ */
 fun Fragment.px2sp(pxValue: Float) = this.resources.px2sp(pxValue)
 
 //---------------
 
-@Desc("Dialog dp转px", "1.0.2")
+/**
+ * Dialog -- dp转px
+ *
+ * @since 1.0.2
+ */
 fun Dialog.dp2px(value: Float) = this.context.dp2px(value)
 
-@Desc("Dialog sp转px", "1.0.2")
+/**
+ * Dialog -- sp2px
+ *
+ * @since 1.0.2
+ */
 fun Dialog.sp2px(value: Float) = this.context.sp2px(value)
 
-@Desc("Context px转dp", "1.0.4")
+/**
+ * Dialog -- px2dp
+ *
+ * @since 1.0.4
+ */
 fun Dialog.px2dp(pxValue: Float) = this.context.px2dp(pxValue)
 
-@Desc("Context px转sp", "1.0.4")
+/**
+ * Dialog -- px转sp
+ *
+ * @since 1.0.4
+ */
 fun Dialog.px2sp(pxValue: Float) = this.context.px2sp(pxValue)
 
 //---------------
 
-@Desc("Number dp转px", "1.0.0", "1.0.2", "使用TypedValue")
+/**
+ * Number -- dp转px
+ *
+ * @since 1.0.0
+ */
 val Float.dp: Float
     get() = Resources.getSystem().dp2px(this)
 
+/**
+ * Number -- dp转px
+ *
+ * @since 1.0.0
+ */
 val Int.dp: Int
     get() = this.toFloat().dp.toInt()
 
+/**
+ * Number -- dp转px
+ *
+ * @since 1.0.0
+ */
 val Long.dp: Long
     get() = this.toFloat().dp.toLong()
 
+/**
+ * Number -- dp转px
+ *
+ * @since 1.0.0
+ */
 val Double.dp: Double
     get() = this.toFloat().dp.toDouble()
 
 
-@Desc("Number sp转px", "1.0.0", "1.0.2", "使用TypedValue")
+/**
+ * Number -- sp转px
+ *
+ * @since 1.0.0
+ */
 val Float.sp: Float
     get() = Resources.getSystem().sp2px(this)
 
+/**
+ * Number -- sp转px
+ *
+ * @since 1.0.0
+ */
 val Int.sp: Int
     get() = this.toFloat().sp.toInt()
 
+/**
+ * Number -- sp转px
+ *
+ * @since 1.0.0
+ */
 val Long.sp: Long
     get() = this.toFloat().sp.toLong()
 
+/**
+ * Number -- sp转px
+ *
+ * @since 1.0.0
+ */
 val Double.sp: Double
     get() = this.toFloat().sp.toDouble()
 
