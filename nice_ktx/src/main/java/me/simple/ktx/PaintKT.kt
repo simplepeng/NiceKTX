a36895f566442dec7498ff50da8603c15bedf69a
simple <383559698@qq.com>
2024-05-10 16:39:19 +0800

    update PaintKT.kt

--- nice_ktx/src/main/java/me/simple/ktx/PaintKT.kt
+++ nice_ktx/src/main/java/me/simple/ktx/PaintKT.kt
@@ -1,16 +1,23 @@
 package me.simple.ktx
 
 import android.graphics.Paint
-import ktx.common.Desc
 import kotlin.math.abs
 
-@Desc("文本高度", "v1.0.4")
+/**
+ * 文本高度
+ *
+ * @since 1.0.4
+ */
 val Paint.textHeight: Float
     get() = with(fontMetrics) {
         abs(top) + abs(bottom)
     }
 
-@Desc("文本ascent高度", "v1.0.4")
+/**
+ * 文本ascent高度
+ *
+ * @since 1.0.4
+ */
 val Paint.textAscentHeight: Float
     get() = with(fontMetrics) {
         abs(ascent) + abs(descent)
